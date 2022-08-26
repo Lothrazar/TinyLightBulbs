1e4571e768b67b61013737eb9fb562ad65776a1f
lothrazar <samson.bassett@gmail.com>
2022-08-26 15:23:20 -0700

    server tested. fix AABBs

--- src/main/java/com/lothrazar/tinylightbulbs/block/BlockBulbPanel.java
+++ src/main/java/com/lothrazar/tinylightbulbs/block/BlockBulbPanel.java
@@ -14,12 +14,12 @@ public class BlockBulbPanel extends BlockBulb {
 
   private static final double SIZE = 0;
   private static final double HGT = 0.3D;
-  private static final VoxelShape CEILING_AABB = Block.box(SIZE, HGT, SIZE, 16 - SIZE, 16.0D, 16 - SIZE);
-  private static final VoxelShape FLOOR_AABB = Block.box(SIZE, 0.0D, SIZE, 16 - SIZE, HGT, 16 - SIZE);
-  private static final VoxelShape SOUTH_AABB = Block.box(SIZE, SIZE, 16 - HGT, 16 - SIZE, 16 - SIZE, 16.0D);
-  private static final VoxelShape NORTH_AABB = Block.box(SIZE, SIZE, 0.0D, 16 - SIZE, 16 - SIZE, HGT);
-  private static final VoxelShape EAST_AABB = Block.box(16 - HGT, SIZE, SIZE, 16.0D, 16 - SIZE, 16 - SIZE);
-  private static final VoxelShape WEST_AABB = Block.box(0.0D, SIZE, SIZE, HGT, 16 - SIZE, 16 - SIZE);
+  private static final VoxelShape CEILING_AABB = Block.box(SIZE, 16 - HGT, SIZE, 16 - SIZE, 16, 16 - SIZE);
+  private static final VoxelShape FLOOR_AABB = Block.box(SIZE, 0, SIZE, 16 - SIZE, HGT, 16 - SIZE);
+  private static final VoxelShape SOUTH_AABB = Block.box(SIZE, SIZE, 16 - HGT, 16 - SIZE, 16 - SIZE, 16);
+  private static final VoxelShape NORTH_AABB = Block.box(SIZE, SIZE, 0, 16 - SIZE, 16 - SIZE, HGT);
+  private static final VoxelShape EAST_AABB = Block.box(16 - HGT, SIZE, SIZE, 16, 16 - SIZE, 16 - SIZE);
+  private static final VoxelShape WEST_AABB = Block.box(0, SIZE, SIZE, HGT, 16 - SIZE, 16 - SIZE);
 
   public BlockBulbPanel(Properties prop, BlockFlib.Settings s) {
     super(prop, s);
